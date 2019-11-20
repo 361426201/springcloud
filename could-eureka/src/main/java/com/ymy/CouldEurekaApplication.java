ff8d32abc9d8349f6782581f14cdeba98600bf64
361426201@qq.com <361426201@qq.com>
2019-11-20 17:36:33 +0800

    update

--- could-eureka/src/main/java/com/ymy/CouldEurekaApplication.java
+++ could-eureka/src/main/java/com/ymy/CouldEurekaApplication.java
@@ -2,8 +2,10 @@ package com.ymy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //开启eureka的注解，一定要加这个注解，否者没有效果
 public class CouldEurekaApplication {
 
     public static void main(String[] args) {
