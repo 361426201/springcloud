82947d768513685b9336c1a604c0ab87bc253f40
361426201@qq.com <361426201@qq.com>
2019-11-20 15:04:46 +0800

    init

---
+++ could-eureka/src/test/java/com/ymy/CouldEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouldEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
