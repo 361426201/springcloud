1f55c9d066d6eb65e99f35ac3bc6a1994d2ba30a
361426201@qq.com <361426201@qq.com>
2019-11-28 12:00:56 +0800

    init

---
+++ cloud-order/src/main/java/com/ymy/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.ymy.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+
+    @Value("${test.name}")
+    private String name;
+
+    @GetMapping(value = "/test")
+    public String test(){
+
+        return "hello："+name+"     人生总是起起落落落落落落落落落落落落落落落落落落落落落落落落落落落落落落落落！";
+    }
+}
